70db875ee452da1e45763b74302a6a138555da53
Justin Ndwiga <ndwigajustin@gmail.com>
2018-10-09 19:18:42 +0300

    [#161066997] listen for status codes

--- UI/js/fetchFullMenu.js
+++ UI/js/fetchFullMenu.js
@@ -8,46 +8,54 @@ window.onload = function getFullMenu(){
             }
     
         })
-    .then(res => res.json())
+    .then(res => {
+        statusCode = res.status
+        return res.json()
+    })
     .then((data) => {
-        let snacksOutput = '';
-        let mainOutput = '';
-        let drinksOutput = '';
-        data['Full Menu'].forEach((menu) => {
-            if (menu.category == 'main'){
-                mainOutput +=`
-                <tr>
-                <td><img src="./images/beefsteak.jpeg"></td>
-                <td>${menu.name}</td>
-                <td>${menu.price}</td>
-                <td><button id="order" class="call_to" onclick="location.href='order.html';">Order</button></td>
-                </tr>
-                `;
-            }
-            if (menu.category == 'snacks'){
-                snacksOutput +=`
-                <tr>
-                <td><img src="./images/sausage.jpeg"></td>
-                <td>${menu.name}</td>
-                <td>${menu.price}</td>
-                <td><button id="order" class="call_to" onclick="location.href='order.html';">Order</button></td>
-                </tr>
-                `;
-            }
-            if (menu.category == 'drinks'){
-                drinksOutput +=`
-                <tr>
-                <td><img src="./images/smoothie.jpeg"></td>
-                <td>${menu.name}</td>
-                <td>${menu.price}</td>
-                <td><button id="order" class="call_to" onclick="location.href='order.html';">Order</button></td>
-                </tr>
-                `;
-            }
-        });   
-        document.getElementById('main-dish').innerHTML = mainOutput;
-        document.getElementById('snacks').innerHTML = snacksOutput;
-        document.getElementById('drinks').innerHTML = drinksOutput;
+        if (statusCode == 200){
+            let snacksOutput = '';
+            let mainOutput = '';
+            let drinksOutput = '';
+            data['Full Menu'].forEach((menu) => {
+                if (menu.category == 'main'){
+                    mainOutput +=`
+                    <tr>
+                    <td><img src="./images/beefsteak.jpeg"></td>
+                    <td>${menu.name}</td>
+                    <td>${menu.price}</td>
+                    <td><button id="order" class="call_to" onclick="location.href='order.html';">Order</button></td>
+                    </tr>
+                    `;
+                }
+                if (menu.category == 'snacks'){
+                    snacksOutput +=`
+                    <tr>
+                    <td><img src="./images/sausage.jpeg"></td>
+                    <td>${menu.name}</td>
+                    <td>${menu.price}</td>
+                    <td><button id="order" class="call_to" onclick="location.href='order.html';">Order</button></td>
+                    </tr>
+                    `;
+                }
+                if (menu.category == 'drinks'){
+                    drinksOutput +=`
+                    <tr>
+                    <td><img src="./images/smoothie.jpeg"></td>
+                    <td>${menu.name}</td>
+                    <td>${menu.price}</td>
+                    <td><button id="order" class="call_to" onclick="location.href='order.html';">Order</button></td>
+                    </tr>
+                    `;
+                }
+            });   
+            document.getElementById('main-dish').innerHTML = mainOutput;
+            document.getElementById('snacks').innerHTML = snacksOutput;
+            document.getElementById('drinks').innerHTML = drinksOutput;
+        }
+        else{
+            document.getElementById('title').innerHTML = res.Message;
+        }
     })
     .catch((err)=>console.log(err))
 }
