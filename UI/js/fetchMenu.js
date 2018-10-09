ce99ede096a8479900e157105eee1ec452fb906c
Justin Ndwiga <ndwigajustin@gmail.com>
2018-10-09 16:35:33 +0300

    [#161066997] allocate every item to respective menu category

--- UI/js/fetchMenu.js
+++ UI/js/fetchMenu.js
@@ -10,19 +10,47 @@ window.onload = function getMenu(){
         })
     .then(res => res.json())
     .then((data) => {
-        let output = '';
+        let snacksOutput = '';
+        let mainOutput = '';
+        let drinksOutput = '';
         data['Full Menu'].forEach(function(menu){
-            output +=`
-            <div class="item">
-            <a href="#content"><img src="./images/juice.jpeg"></a>
-            <h3>${menu.name}</h3>
-            <p> ${menu.name} for your ${menu.category} meal </p>
-            <b>KES. ${menu.price}</b><br><br>
-            <button class="call_to" onclick="location.href='order.html';">Order Now</button>
-            </div>
-            `;
+            if (menu.category == 'main'){
+                mainOutput +=`
+                <div class="item">
+                <a href="#content"><img src="./images/beefsteak.jpeg"></a>
+                <h3>${menu.name}</h3>
+                <p> ${menu.name} for that heavy meal </p>
+                <b>KES. ${menu.price}</b><br><br>
+                <button class="call_to" onclick="location.href='order.html';">Order Now</button>
+                </div>
+                `;
+            }
+            if (menu.category == 'snacks'){
+                snacksOutput +=`
+                <div class="item">
+                <a href="#content"><img src="./images/hamburger.jpeg"></a>
+                <h3>${menu.name}</h3>
+                <p> ${menu.name} for your grab and go meal </p>
+                <b>KES. ${menu.price}</b><br><br>
+                <button class="call_to" onclick="location.href='order.html';">Order Now</button>
+                </div>
+                `;
+            }
+            if (menu.category == 'drinks'){
+                drinksOutput +=`
+                <div class="item">
+                <a href="#content"><img src="./images/coffee..jpeg"></a>
+                <h3>${menu.name}</h3>
+                <p> ${menu.name} for your thirst </p>
+                <b>KES. ${menu.price}</b><br><br>
+                <button class="call_to" onclick="location.href='order.html';">Order Now</button>
+                </div>
+                `;
+            }
         });   
-        document.getElementById('main-dish').innerHTML = output;
+        document.getElementById('main-dish').innerHTML = mainOutput;
+        document.getElementById('snacks').innerHTML = snacksOutput;
+        document.getElementById('drinks').innerHTML = drinksOutput;
     })
     .catch((err)=>console.log(err))
 }
