23a36a29ec84e4638dc5b5e402739c35b0256114
Justin Ndwiga <ndwigajustin@gmail.com>
2018-10-11 12:26:41 +0300

    [#161126324] post single order

--- UI/js/fetchMenu.js
+++ UI/js/fetchMenu.js
@@ -14,47 +14,124 @@ window.onload = function getMenu(){
     })
     .then((data) => {
         if (statusCode == 200){        
-            let snacksOutput = '';
-            let mainOutput = '';
-            let drinksOutput = '';
             data['Full Menu'].forEach((menu) => {
                 if (menu.category == 'main'){
-                    mainOutput +=`
-                    <div class="item">
-                    <a href="#content"><img src="./images/beefsteak.jpeg"></a>
-                    <h3>${menu.name}</h3>
-                    <p> ${menu.name} for that heavy meal </p>
-                    <b>KES. ${menu.price}</b><br><br>
-                    <button class="call_to" onclick="location.href='order.html';">Order Now</button>
-                    </div>
-                    `;
+                    mainItems = document.createElement('div');
+                    mainItems.setAttribute('class', 'item');
+                    let link = document.createElement('a');
+                    link.setAttribute('href', '#content');
+                    let image = document.createElement('img');
+                    image.setAttribute('src', menu.image);
+                    link.appendChild(image);
+                    let name = document.createElement('h3');
+                    let nameText = document.createTextNode(menu.name);
+                    name.appendChild(nameText);
+                    let description = document.createElement('p');
+                    let descriptionText = document.createTextNode(menu.name + " for your grab and go meal");
+                    description.appendChild(descriptionText);
+                    let price = document.createElement('b');
+                    let priceText = document.createTextNode('KES '+ menu.price);
+                    price.appendChild(priceText);
+                    let br = document.createElement('br');
+                    let button = document.createElement('button');
+                    button.setAttribute('class', "call_to");
+                    button.setAttribute('id', menu.name)
+                    let buttonText = document.createTextNode('Order Now')
+                    button.appendChild(buttonText)
+                    button.addEventListener('click', function clicked(){
+                        localStorage.removeItem('orderItem');
+                        localStorage.setItem('orderItem', this.id)
+                    })
+                    button.addEventListener('click', postOrder);
+                    mainItems.appendChild(link);
+                    mainItems.appendChild(name);
+                    mainItems.appendChild(description);
+                    mainItems.appendChild(price);
+                    mainItems.appendChild(br);
+                    br2 = br.cloneNode(true)
+                    mainItems.appendChild(br2);
+                    mainItems.appendChild(button)
+                    document.getElementById('main-dish').appendChild(mainItems);
                 }
                 if (menu.category == 'snacks'){
-                    snacksOutput +=`
-                    <div class="item">
-                    <a href="#content"><img src="./images/hamburger.jpeg"></a>
-                    <h3>${menu.name}</h3>
-                    <p> ${menu.name} for your grab and go meal </p>
-                    <b>KES. ${menu.price}</b><br><br>
-                    <button class="call_to" onclick="location.href='order.html';">Order Now</button>
-                    </div>
-                    `;
+                    snacksItems = document.createElement('div');
+                    snacksItems.setAttribute('class', 'item');
+                    let link = document.createElement('a');
+                    link.setAttribute('href', '#content');
+                    let image = document.createElement('img');
+                    image.setAttribute('src', menu.image);
+                    link.appendChild(image);
+                    let name = document.createElement('h3');
+                    let nameText = document.createTextNode(menu.name);
+                    name.appendChild(nameText);
+                    let description = document.createElement('p');
+                    let descriptionText = document.createTextNode(menu.name + " for your grab and go meal");
+                    description.appendChild(descriptionText);
+                    let price = document.createElement('b');
+                    let priceText = document.createTextNode('KES '+ menu.price);
+                    price.appendChild(priceText);
+                    let br = document.createElement('br');
+                    let button = document.createElement('button');
+                    button.setAttribute('class', "call_to");
+                    button.setAttribute('id', menu.name)
+                    let buttonText = document.createTextNode('Order Now')
+                    button.appendChild(buttonText)
+                    button.addEventListener('click', function clicked(){
+                        localStorage.removeItem('orderItem');
+                        localStorage.setItem('orderItem', this.id)
+                    })
+                    button.addEventListener('click', postOrder);
+                    snacksItems.appendChild(link);
+                    snacksItems.appendChild(name);
+                    snacksItems.appendChild(description);
+                    snacksItems.appendChild(price);
+                    snacksItems.appendChild(br);
+                    br2 = br.cloneNode(true)
+                    snacksItems.appendChild(br2);
+                    snacksItems.appendChild(button)
+                    document.getElementById('snacks').appendChild(snacksItems);
                 }
                 if (menu.category == 'drinks'){
-                    drinksOutput +=`
-                    <div class="item">
-                    <a href="#content"><img src="./images/coffee..jpeg"></a>
-                    <h3>${menu.name}</h3>
-                    <p> ${menu.name} for your thirst </p>
-                    <b>KES. ${menu.price}</b><br><br>
-                    <button class="call_to" onclick="location.href='order.html';">Order Now</button>
-                    </div>
-                    `;
+                    drinksItems = document.createElement('div');
+                    drinksItems.setAttribute('class', 'item');
+                    let link = document.createElement('a');
+                    link.setAttribute('href', '#content');
+                    let image = document.createElement('img');
+                    image.setAttribute('src', menu.image);
+                    link.appendChild(image);
+                    let name = document.createElement('h3');
+                    let nameText = document.createTextNode(menu.name);
+                    name.appendChild(nameText);
+                    let description = document.createElement('p');
+                    let descriptionText = document.createTextNode(menu.name + " for your grab and go meal");
+                    description.appendChild(descriptionText);
+                    let price = document.createElement('b');
+                    let priceText = document.createTextNode('KES '+ menu.price);
+                    price.appendChild(priceText);
+                    let br = document.createElement('br');
+                    let button = document.createElement('button');
+                    button.setAttribute('class', "call_to");
+                    button.setAttribute('id', 'orderOne')
+                    button.setAttribute('name', menu.name)
+                    button.addEventListener('click', clicked)
+                    let buttonText = document.createTextNode('Order Now')
+                    button.appendChild(buttonText)
+                    button.addEventListener('click', function clicked(){
+                        localStorage.removeItem('orderItem');
+                        localStorage.setItem('orderItem', this.id)
+                    })
+                    button.addEventListener('click', postOrder);
+                    drinksItems.appendChild(link);
+                    drinksItems.appendChild(name);
+                    drinksItems.appendChild(description);
+                    drinksItems.appendChild(price);
+                    drinksItems.appendChild(br);
+                    br2 = br.cloneNode(true)
+                    drinksItems.appendChild(br2);
+                    drinksItems.appendChild(button)
+                    document.getElementById('drinks').appendChild(drinksItems);
                 }
             });   
-            document.getElementById('main-dish').innerHTML = mainOutput;
-            document.getElementById('snacks').innerHTML = snacksOutput;
-            document.getElementById('drinks').innerHTML = drinksOutput;
         }
         else {
             document.getElementById('content').innerHTML = res.Message;
---
+++ UI/js/postOrder.js
@@ -0,0 +1,29 @@
+const orderURL = 'https://fastfoodfast-api.herokuapp.com/api/v2/auth/users/orders';
+
+function postOrder(){
+    let item = localStorage.getItem('orderItem');
+    let token = localStorage.getItem('token');
+    items = {}
+    items[item] = 1
+    order = JSON.stringify({
+        'items':items
+    })
+    fetch(orderURL, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json',
+            'x-access-token': token
+        },
+        body: order
+    })
+    .then(res =>{
+        statusCode = res.status
+        return res.json()
+    })
+    .then(data =>{
+        console.log(data)
+        alert(data.Message)
+    })
+    .catch(error => console.log(err))
+}
