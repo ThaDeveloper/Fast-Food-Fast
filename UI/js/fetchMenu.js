2f9819b6f1a5532587a3db4ee1b6b9c014e6d774
Justin Ndwiga <ndwigajustin@gmail.com>
2018-10-12 02:35:53 +0300

    [#161126324] add order items to cart

--- UI/js/fetchMenu.js
+++ UI/js/fetchMenu.js
@@ -1,4 +1,5 @@
 const baseURL = 'https://fastfoodfast-api.herokuapp.com/api/v2/menu'
+window.orderItems = []
 window.onload = function getMenu(){
     fetch(baseURL, {
             method: 'GET',
@@ -77,10 +78,11 @@ window.onload = function getMenu(){
                     let buttonText = document.createTextNode('Order Now')
                     button.appendChild(buttonText)
                     button.addEventListener('click', function clicked(){
-                        localStorage.removeItem('orderItem');
-                        localStorage.setItem('orderItem', this.id)
+                        orderItems.push({'name':this.id,'price':menu.price, 'image':menu.image})
+                        localStorage.setItem('orderItems', JSON.stringify(orderItems))
+                        console.log(localStorage.getItem('orderItems'))
+                        alert('Item added to cart')
                     })
-                    button.addEventListener('click', postOrder);
                     snacksItems.appendChild(link);
                     snacksItems.appendChild(name);
                     snacksItems.appendChild(description);
